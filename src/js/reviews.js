2dd7993b4b48ea2a12516355a6bb1abbb598bffc
Oleksandr <gayrsuk@gmail.com>
2024-10-20 02:18:03 +0300

    added correct slider to mobile layout

--- src/js/reviews.js
+++ src/js/reviews.js
@@ -1,29 +1,30 @@
-import { Swiper } from 'swiper';
+import Swiper from 'swiper/bundle';
+
+import 'swiper/css/bundle';
 
 const swiper = new Swiper('.reviews-swiper', {
   direction: 'horizontal',
-  loop: true,
+  loop: false,
   slidesPerView: 1,
   spaceBetween: 30,
-  loopedSlides: 3,
+  centeredSlides: false,
+  watchSlidesProgress: true,
+  watchSlidesVisibility: true,
   scrollbar: {
     el: '.swiper-scrollbar',
     draggable: true,
-  },
-  autoplay: {
-    delay: 5000,
   },
   navigation: {},
   breakpoints: {
     768: {
       slidesPerView: 2,
       spaceBetween: 20,
-      loopedSlides: 2,
+      centeredSlides: false,
     },
     1024: {
       slidesPerView: 3,
       spaceBetween: 30,
-      loopedSlides: 3,
+      centeredSlides: false,
     },
   },
 });
